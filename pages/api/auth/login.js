6fa36a7c24ea39c06b55e9a0d5e35773ea683324
ANormalDude <wasim.khan202000.ib@gmail.com>
2023-09-16 21:08:13 +0100

    Update login.js

--- pages/api/auth/login.js
+++ pages/api/auth/login.js
@@ -8,25 +8,29 @@ const JWT_SECRET = process.env.JWT_SECRET;
 const MONGODB_URI = process.env.MONGODB_URI;
 
 export default async (req, res) => {
-  if (req.method === 'POST') {
-    const { username, password } = req.body;
-
-    const client = new MongoClient(MONGODB_URI);
-    await client.connect();
-    const db = client.db('Cluster0');
-    const user = await db.collection('users').findOne({ username });
-
-    if (user && bcrypt.compareSync(password, user.password)) {
-      const token = jwt.sign({ id: user._id }, JWT_SECRET, {
-        expiresIn: 86400 // expires in 24 hours
-      });
-      res.status(200).send({ auth: true, token });
+  try {
+    if (req.method === 'POST') {
+      const { email, password } = req.body;
+  
+      const client = new MongoClient(MONGODB_URI);
+      await client.connect();
+      const db = client.db('chatbotDB');
+      const user = await db.collection('users').findOne({ email });
+      
+      if (user && bcrypt.compareSync(password, user.password)) {
+        const token = jwt.sign({ id: user._id }, JWT_SECRET, {
+          expiresIn: 86400 // expires in 24 hours
+        });
+        res.status(200).send({ auth: true, token });
+      } else {
+        res.status(401).send({ auth: false, token: null });
+      }
+  
+      await client.close();
     } else {
-      res.status(401).send({ auth: false, token: null });
+      res.status(405).end(); // Method Not Allowed
     }
-
-    await client.close();
-  } else {
-    res.status(405).end(); // Method Not Allowed
+  } catch (error) {
+    console.log(error)
   }
 };
