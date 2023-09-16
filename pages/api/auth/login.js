849ab5b923d7232188b53ff24564da0e6039c7d5
ANormalDude <wasim.khan202000.ib@gmail.com>
2023-09-16 20:13:25 +0100

    connect to back-end first attempt

---
+++ pages/api/auth/login.js
@@ -0,0 +1,32 @@
+// pages/api/login.js
+
+import { MongoClient } from 'mongodb';
+import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
+
+const JWT_SECRET = process.env.JWT_SECRET;
+const MONGODB_URI = process.env.MONGODB_URI;
+
+export default async (req, res) => {
+  if (req.method === 'POST') {
+    const { username, password } = req.body;
+
+    const client = new MongoClient(MONGODB_URI);
+    await client.connect();
+    const db = client.db('Cluster0');
+    const user = await db.collection('users').findOne({ username });
+
+    if (user && bcrypt.compareSync(password, user.password)) {
+      const token = jwt.sign({ id: user._id }, JWT_SECRET, {
+        expiresIn: 86400 // expires in 24 hours
+      });
+      res.status(200).send({ auth: true, token });
+    } else {
+      res.status(401).send({ auth: false, token: null });
+    }
+
+    await client.close();
+  } else {
+    res.status(405).end(); // Method Not Allowed
+  }
+};
